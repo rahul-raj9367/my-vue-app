abbb57973273437228a131ad29af8dee65912a14
Rahul Raj <rahulselvan0810.com>
2023-10-11 10:49:46 +0530

    configuracoes de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
+import About from './pages/About';
+import Contactus from './pages/Contactus';
+import PageError from './pages/PageError';
+import ChengalpattuRice from './pages/ChengalpattuRice';
+import DoubleBoiled from './pages/DoubleBoiled';
+import IdlyRice from './pages/IdlyRice';
+import PoniRice from './pages/PoniRice';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    <Navbar/>
+    <ScrollToTop />
+     <Routes>
+        <Route index path="/my-vue-app/" element={<Home />} />
+        <Route path="/my-vue-app/About" element={<About />} />
+        <Route path="/my-vue-app/ContactUs" element={<Contactus />} />
+        <Route path="/my-vue-app/ChengalpattuRice" element={<ChengalpattuRice />} />
+        <Route path="/my-vue-app/DoubleBoiled" element={<DoubleBoiled />} />
+        <Route path="/my-vue-app/IdlyRice" element={<IdlyRice />} />
+        <Route path="/my-vue-app/PoniRice" element={<PoniRice />} />
+        <Route path="*" element={<PageError />} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
   )
 }
 
